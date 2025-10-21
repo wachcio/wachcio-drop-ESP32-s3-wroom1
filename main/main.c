c724f9540cda668363ec476dac7861537331a22a
wachcio <wachcio@poczta.fm>
2025-10-21 03:52:24 +0200

    Blink LED 14 for test

--- main/main.c
+++ main/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#define LED_GPIO 14  // pin na którym testujemy diodę
+static const char *TAG = "WachcioDrop_BLINK";
 
 void app_main(void)
 {
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
 
+    while (true) {
+        gpio_set_level(LED_GPIO, 1);
+        ESP_LOGI(TAG, "LED ON");
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+
+        gpio_set_level(LED_GPIO, 0);
+        ESP_LOGI(TAG, "LED OFF");
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
 }
